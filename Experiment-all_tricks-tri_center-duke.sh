e64856eb1403d09ae5527f919d6dfa7121ddb42e
youzunzhi <youzunzhi@163.com>
2019-08-12 11:31:44 +0800

    multisource config

--- Experiment-all_tricks-tri_center-duke.sh
+++ Experiment-all_tricks-tri_center-duke.sh
@@ -8,4 +8,4 @@
 # last stride 1
 # bnneck on
 # with center loss
-python3 tools/train.py --config_file='configs/softmax_triplet_with_center.yml' MODEL.DEVICE_ID "('1')" DATASETS.NAMES "('dukemtmc')" DATASETS.ROOT_DIR "('/home/share/zunzhi')" OUTPUT_DIR "('/home/zunzhi/reid-strong-baseline/log/reid_baseline_review/Opensource_test/dukemtmc/Experiment-all-tricks-tri_center-256x128-bs16x4-warmup10-erase0_5-labelsmooth_on-laststride1-bnneck_on-triplet_centerloss0_0005')"
+python3 tools/train.py --config_file='configs/softmax_triplet_with_center.yml' MODEL.DEVICE_ID "('1')" DATASETS.ROOT_DIR "('/home/share/zunzhi')" OUTPUT_DIR "('/home/zunzhi/reid-strong-baseline/log/reid_baseline_review/Opensource_test/dukemtmc/Experiment-all-tricks-tri_center-256x128-bs16x4-warmup10-erase0_5-labelsmooth_on-laststride1-bnneck_on-triplet_centerloss0_0005')"
